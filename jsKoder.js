69af6dcf626bd7662f82223d977a1c3d2b413cfe
FormTestX <83898092+FormTestX@users.noreply.github.com>
2021-10-09 20:07:58 +0800

    Delete jsKoder.js

--- jsKoder.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.getElementsById('menuicon')[0]
-const navBar = document.getElementsByClassName('ul')[0]
-
-
-menuicon.addEventListener('click', () =>{
-    navBar.classList.toggle('active')
-})
-
